19a111d2be97906a03a1255bae601741810d2746
vladovoev <vladovoev@yahoo.co.uk>
2018-02-24 23:30:13 +0200

    fixed bridge patterns and addes class diagrams

--- Patterns/BridgeDemo/Startup.cs
+++ Patterns/BridgeDemo/Startup.cs
@@ -1,6 +1,4 @@
-﻿using System;
-
-namespace BridgeDemo
+﻿namespace BridgeDemo
 {
     class Startup
     {
@@ -11,43 +9,4 @@ namespace BridgeDemo
             Bridgedemo.ThirdDemo();
         }
     }
-
-    public abstract class Living
-    {
-        public IPresenter presenter { get; set; }
-        public int Age { get; set; }
-        public string Name { get; set; }
-        public abstract void Talk();
-    }
-
-    public interface IPresenter
-    {
-        void Introduce(int age, string name);
-    }
-
-    public class Cat : Living
-    {
-        public override void Talk()
-        {
-            presenter.Introduce(Age, Name);
-        }
-    }
-
-    public class Tiger : IPresenter
-    {
-        public void Introduce(int age, string name)
-        {
-            Console.WriteLine($"I am a big bad {this.GetType().Name} with name:{name} and years{age}");
-        }
-    }
-
-    public class Zebra : IPresenter
-    {
-        public void Introduce(int age, string name)
-        {
-            Console.WriteLine($"I like to eat grass {this.GetType().Name} with name:{name} and years{age}");
-        }
-    }
-
-
 }
