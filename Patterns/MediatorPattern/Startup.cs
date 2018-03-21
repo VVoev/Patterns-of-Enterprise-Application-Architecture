742d5617346a0b773760ebe902e465f339019020
Vladimir Yonkov Voev <Vladimir.Voev@allocatesoftware.com>
2018-03-21 23:33:35 +0200

    Patterns

---
+++ Patterns/MediatorPattern/Startup.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediatorPattern
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            var flight1 = new Airbus321();
+            var boing2 = new Boing2();
+            var stealth3 = new Stealth3();
+        }
+
+     
+    }
+
+    public abstract class Aicraft
+    {
+        private int currentAttitude;
+        private readonly IAirTraficControl airTraficControl;
+        protected IList<Aicraft> acknowledgedAicraft = new List<Aicraft>();
+
+        protected Aicraft(string callSign,IAirTraficControl airTraficControl)
+        {
+            this.CallSign = callSign;
+            this.airTraficControl = airTraficControl;
+        }
+
+        public void Acknowledge(Aicraft aicraft)
+        {
+            acknowledgedAicraft.Add(aicraft);
+        }
+
+        public abstract int Ceiling { get; }
+
+        public abstract bool isTrailingGapAcceptable();
+
+        public string CallSign { get; }
+
+        public int Atitude
+        {
+            get
+            {
+                return this.currentAttitude;
+            }
+        }
+    }
+
+    public class Airbus321 : Aicraft
+    {
+        public Airbus321(string callsign) : base(callsign)
+        {
+        }
+
+        public override int Ceiling => 41000;
+
+        public override bool isTrailingGapAcceptable()
+        {
+            foreach (var acknowledgedAicraft in this.acknowledgedAicraft)
+            {
+                if(acknowledgedAicraft.GetType() == typeof(Boing2))
+                {
+
+                }
+
+                if (acknowledgedAicraft.GetType() == typeof(Stealth3))
+                {
+
+                }
+            }
+
+            return false;
+        }
+    }
+
+    public class Boing2
+    {
+        public Boing2()
+        {
+        }
+    }
+    public class Stealth3
+    {
+        public Stealth3()
+        {
+        }
+    }
+
+    public interface IAirTraficControl
+    {
+        void ReceiveAircraftLocation(Aicraft location);
+        void RegisterAircraftUnderGuidance(Aicraft aicraft);
+    }
+}
---
+++ Patterns/TemplatePattern/Startup.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemplatePattern
+{
+    class Startup
+    {
+        static void Main(string[] args)
+        {
+            FirstDemo();
+            SecondDemo();
+        }
+
+        private static void SecondDemo()
+        {
+            DataExporter exporter = null;
+
+            // Lets export the data to Excel file
+            exporter = new ExcelExporter();
+            exporter.ExportFormatedData();
+
+            Console.WriteLine();
+
+            // Lets export the data to PDF file
+            exporter = new PDFExporter();
+            exporter.ExportFormatedData();
+        }
+
+        private static void FirstDemo()
+        {
+            AbstractAlgorithmSkeleton objTemplate = null;
+            objTemplate = new ConcreteClassA();
+            // Now this call to TemplateMethod() will direct calls
+            // to methods in ConcreteClassA.
+            objTemplate.TemplateMethod();
+        }
+    }
+    
+    abstract class DataExporter
+    {
+        // This will not vary as the data is read from sql only
+        public void ReadData()
+        {
+            Console.WriteLine("Reading the data from SqlServer");
+        }
+
+        // This will not vary as the format of report is fixed.
+        public void FormatData()
+        {
+            Console.WriteLine("Formating the data as per requriements.");
+        }
+
+        // This may vary based on target file type choosen
+        public abstract void ExportData();
+
+        // This is the template method that the client will use.
+        public void ExportFormatedData()
+        {
+            this.ReadData();
+            this.FormatData();
+            this.ExportData();
+        }
+    }
+    class ExcelExporter : DataExporter
+    {
+        public override void ExportData()
+        {
+            Console.WriteLine("Exporting the data to an Excel file.");
+        }
+    }
+    class PDFExporter : DataExporter
+    {
+        public override void ExportData()
+        {
+            Console.WriteLine("Exporting the data to a PDF file.");
+        }
+    }
+}
+
+#region FirstDemo
+public abstract class AbstractAlgorithmSkeleton
+{
+    public void TemplateMethod()
+    {
+        // Template Method declaring algorithm
+        // in terms of abstract operations.
+        Step1();
+        Step2();
+        Step3();
+    }
+    public abstract void Step1();
+    public abstract void Step2();
+    public abstract void Step3();
+}
+
+public class ConcreteClassA : AbstractAlgorithmSkeleton
+{
+    public override void Step1()
+    {
+        Console.WriteLine("ConcreteClassA, Step 1");
+    }
+
+    public override void Step2()
+    {
+        Console.WriteLine("ConcreteClassA, Step 2");
+    }
+
+    public override void Step3()
+    {
+        Console.WriteLine("ConcreteClassA, Step 3");
+    }
+
+    public void OtherMethodA()
+    {
+        //
+    }
+}
+
+public class ConcreteClassB : AbstractAlgorithmSkeleton
+{
+    public override void Step1()
+    {
+        Console.WriteLine("ConcreteClassB, Step 1");
+    }
+
+    public override void Step2()
+    {
+        Console.WriteLine("ConcreteClassB, Step 2");
+    }
+
+    public override void Step3()
+    {
+        Console.WriteLine("ConcreteClassB, Step 3");
+    }
+
+    public void OtherMethodB()
+    {
+        //
+    }
+    #endregion
+}
+
